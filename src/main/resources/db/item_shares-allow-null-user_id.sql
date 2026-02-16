ceab5653b21881677b64ebafc3bc63330bc99e37
Cleverton Decker <cleverton_decker@hotmail.com>
2026-02-15 22:02:46 -0300

    chore: remove ItemShareSchemaMigration bean and SQL script

--- src/main/resources/db/item_shares-allow-null-user_id.sql
+++
@@ -1,4 +0,0 @@
--- Allow user_id to be NULL so item shares can be assigned to trusted contacts (no system user).
--- Run this if Hibernate ddl-auto=update did not alter the column (e.g. MySQL).
--- MySQL:
-ALTER TABLE item_shares MODIFY COLUMN user_id BIGINT NULL;
